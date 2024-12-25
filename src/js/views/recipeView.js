eaf015543dcc84d2c2b505a48b82506ceb0c2b41
Tasmachi <ishamsiyev094@gmail.com>
2024-12-26 01:13:20 +0500

    fix fractional issue

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,29 +1,30 @@
 import icons from 'url:../../img/icons.svg';
-import { Fraction } from 'fractional';
+import Fraction from 'fractional';
+
 import View from './view.js';
 
-class RecipeView extends View{
+class RecipeView extends View {
   parentElement = document.querySelector('.recipe');
-  successmessage='';
+  successmessage = '';
   errormessage = 'We could not find that recipe .Please try another one!';
   addHandlerRender(handler) {
     ['hashchange', 'load'].forEach(ev => window.addEventListener(ev, handler));
   }
 
-  addHandlerUpdateServings(handler){
-    this.parentElement.addEventListener('click',function(e){
-      const btn=e.target.closest('.btn--update-servings')
-      if(!btn) return;
+  addHandlerUpdateServings(handler) {
+    this.parentElement.addEventListener('click', function (e) {
+      const btn = e.target.closest('.btn--update-servings');
+      if (!btn) return;
       const updateTo = +btn.dataset.updateTo;
-      
-      if(updateTo>0) handler(updateTo)
-    })
-  };
 
-  addHandlerAddBookmark(handler){
-    this.parentElement.addEventListener('click',function(e){
-      const btn=e.target.closest('.btn--bookmark');
-      if(!btn) return;
+      if (updateTo > 0) handler(updateTo);
+    });
+  }
+
+  addHandlerAddBookmark(handler) {
+    this.parentElement.addEventListener('click', function (e) {
+      const btn = e.target.closest('.btn--bookmark');
+      if (!btn) return;
       handler();
     });
   }
@@ -59,12 +60,16 @@ class RecipeView extends View{
           <span class="recipe__info-text">servings</span>
 
           <div class="recipe__info-buttons">
-            <button class="btn--tiny btn--update-servings" data-update-to="${this.data.servings-1}">
+            <button class="btn--tiny btn--update-servings" data-update-to="${
+              this.data.servings - 1
+            }">
               <svg>
                 <use href="${icons}#icon-minus-circle"></use>
               </svg>
             </button>
-            <button class="btn--tiny btn--update-servings" data-update-to="${this.data.servings+1}">
+            <button class="btn--tiny btn--update-servings" data-update-to="${
+              this.data.servings + 1
+            }">
               <svg>
                 <use href="${icons}#icon-plus-circle"></use>
               </svg>
@@ -72,14 +77,16 @@ class RecipeView extends View{
           </div>
         </div>
 
-        <div class="recipe__user-generated ${this.data.key?'':'hidden'}">
+        <div class="recipe__user-generated ${this.data.key ? '' : 'hidden'}">
             <svg>
               <use href="${icons}#icon-user"></use>
             </svg>
         </div>
         <button class="btn--round btn--bookmark">
           <svg class="">
-            <use href="${icons}#icon-bookmark${this.data.bookmarked==true?'-fill':''}"></use>
+            <use href="${icons}#icon-bookmark${
+      this.data.bookmarked == true ? '-fill' : ''
+    }"></use>
           </svg>
         </button>
       </div>
